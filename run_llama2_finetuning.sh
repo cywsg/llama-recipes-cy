b2cb2e2bef51f02b8ea6732c429141c56ed22dbc
cywee <wee_chong_yaw@imda.gov.sg>
2023-08-14 06:28:00 +0000

    add run scripts

---
+++ run_llama2_finetuning.sh
@@ -0,0 +1,9 @@
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+export ACCELERATE_USE_FSDP=true
+
+torchrun --nnodes 1 --nproc_per_node 8  llama_finetuning.py --enable_fsdp  \
+	 --model_name  /home/zhongweili/work/models/Llama-2-70b-hf  \
+	 --dist_checkpoint_root_folder model_checkpoints \
+	 --dist_checkpoint_folder sso-finetuned-4k  --pure_bf16 --use_fast_kernels \
+	 --dataset ssoj_dataset --num_epochs 1 --lr 0.000002 \
+	 --weight_decay 0.01   --batch_size_training 1 --micro_batch_size 1 
---
+++ run_llama_convert_model.sh
@@ -0,0 +1,6 @@
+
+python llama_convert_model.py \
+	-base_model_path /home/zhongweili/work/models/Llama-2-13b-hf \
+	-finetune_dir model_checkpoints/sso-finetuned-4k-/home/zhongweili/work/models/Llama-2-13b-hf \
+	-pt Llama-2-13b-hf-0.pt \
+	-save_output /home/cywee/work/models/save_model_llama2/4k/Llama-2-13b-hf-0
